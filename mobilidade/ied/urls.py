4eea3a3e9f54161f3d1860109e58f52235dcab24
unknown <vitoriaguidine@id.uff.br>
2023-07-31 16:58:23 -0300

    passando o html para django

--- mobilidade/ied/urls.py
+++ mobilidade/ied/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.urls import path
+from ied import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('admin/', admin.site.urls),
+    path('friends/', views.friends_view, name='friends')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mobilidade/ied/views.py
+++ mobilidade/ied/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("primeiro teste")
+def friends_view(request):
+    return render(request, 'mobilidade/templates/page.html')
 
 # Create your views here.
